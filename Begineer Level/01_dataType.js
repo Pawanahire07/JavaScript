2a33aaa555595c819db2051ccbb9efd3314d6330
Pawan <Pawan@techjar.in>
2025-06-26 10:46:31 +0530

    New Changes

--- Begineer Level/01_dataType.js
+++ Begineer Level/01_dataType.js
@@ -1,17 +1,21 @@
-// let name = "Hello, Pawan";
-// console.log(name);
+//String
+let name = "Hello, Pawan";
+console.log(name);
+console.log(typeof name);
 
-// console.log(typeof name);
-
+//null
 let a = null;
 console.log(typeof a);
 
+//undefined
 let b;
 console.log(typeof b);
 
+//bigInt
 let number = 536218293902n;
 console.log(typeof number);
 
+//symbol
 const abc = Symbol("Pawan");
 const def = Symbol("Pawan");
 
